c4d2a11743a8fbae5739e1b273cda7c43974423e
AshwinDevelops <chugh.ashwin@gmail.com>
2019-09-19 20:32:19 -0700

    Update Runner.java

--- Formula Lab/Runner.java
+++ Formula Lab/Runner.java
@@ -12,8 +12,6 @@ public class Runner
                     startGame();
                 if(!playing)
                     return;
-                
-                Thread.sleep(200);
              }
     }
 
